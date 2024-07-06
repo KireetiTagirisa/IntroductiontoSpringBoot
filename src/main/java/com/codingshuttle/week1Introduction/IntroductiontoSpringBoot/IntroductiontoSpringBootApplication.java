2f7a78bc556b539edd34c10ff2e4965290a2d467
Kireeti Tagirisa <kireetitagirisa@gmail.com>
2024-07-06 14:02:13 +0530

    added new features

---
+++ src/main/java/com/codingshuttle/week1Introduction/IntroductiontoSpringBoot/IntroductiontoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.week1Introduction.IntroductiontoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductiontoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductiontoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/week1Introduction/IntroductiontoSpringBoot/IntroductiontoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.week1Introduction.IntroductiontoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductiontoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
